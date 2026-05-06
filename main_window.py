515d749825b3898976e587fb62789d887ed9b4d9
raynaldclark <razorsh5908@sina.com>
2026-05-06 12:33:51 +0800

    Double toolbar icons to 56px

--- main_window.py
+++ main_window.py
@@ -121,7 +121,7 @@ class MainWindow(QMainWindow):
 
         # 显示已完成
         show_done_btn = QPushButton()
-        show_done_btn.setIcon(self._svg_icon("fin.svg", 28))
+        show_done_btn.setIcon(self._svg_icon("fin.svg", 56))
         show_done_btn.setToolTip("显示已完成")
         show_done_btn.setFixedSize(40, 36)
         show_done_btn.setCursor(Qt.PointingHandCursor)
@@ -136,7 +136,7 @@ class MainWindow(QMainWindow):
 
         # 设置按钮
         settings_btn = QPushButton()
-        settings_btn.setIcon(self._svg_icon("setting.svg", 28))
+        settings_btn.setIcon(self._svg_icon("setting.svg", 56))
         settings_btn.setToolTip("设置")
         settings_btn.setFixedSize(40, 36)
         settings_btn.setCursor(Qt.PointingHandCursor)
@@ -151,7 +151,7 @@ class MainWindow(QMainWindow):
 
         # 清空按钮
         clear_done_btn = QPushButton()
-        clear_done_btn.setIcon(self._svg_icon("delfin.svg", 28))
+        clear_done_btn.setIcon(self._svg_icon("delfin.svg", 56))
         clear_done_btn.setToolTip("清空已完成")
         clear_done_btn.setFixedSize(40, 36)
         clear_done_btn.setCursor(Qt.PointingHandCursor)
@@ -163,7 +163,7 @@ class MainWindow(QMainWindow):
         t.addWidget(clear_done_btn)
 
         clear_all_btn = QPushButton()
-        clear_all_btn.setIcon(self._svg_icon("delall.svg", 28))
+        clear_all_btn.setIcon(self._svg_icon("delall.svg", 56))
         clear_all_btn.setToolTip("清空全部")
         clear_all_btn.setFixedSize(40, 36)
         clear_all_btn.setCursor(Qt.PointingHandCursor)
@@ -178,7 +178,7 @@ class MainWindow(QMainWindow):
 
         # 添加按钮
         add_btn = QPushButton()
-        add_btn.setIcon(self._svg_icon("add.svg", 28))
+        add_btn.setIcon(self._svg_icon("add.svg", 56))
         add_btn.setToolTip("添加任务")
         add_btn.setFixedSize(40, 36)
         add_btn.setCursor(Qt.PointingHandCursor)
